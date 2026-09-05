7a512e28a480fcafc3bc1a48567adc4a6c2f3205
Mateo Pineda <Mateo Pineda@DESKTOP-6PR1K67>
2026-09-05 18:31:27 -0500

    Endpoint para crear usuario nuevo

--- src/backend/src/users/users.controller.ts
+++ src/backend/src/users/users.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+
 import { UsersService } from './users.service.js';
+import { User } from './entities/user.entity.js';
+import { CreateUserDto } from './dto/create-user.dto.js';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return await this.usersService.create(createUserDto);
+  }
 }
--- src/backend/src/users/users.service.ts
+++ src/backend/src/users/users.service.ts
@@ -1,7 +1,9 @@
 import { Injectable, BadRequestException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+
 import { User } from './entities/user.entity.js';
+import { CreateUserDto } from './dto/create-user.dto.js';
 
 @Injectable()
 export class UsersService {
@@ -12,5 +14,20 @@ export class UsersService {
 
   async findOne(username: string): Promise<User | null> {
     return await this.usersRepository.findOneBy({ username });
+  }
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const existingUser = await this.findOne(createUserDto.username);
+
+    if (existingUser) {
+      throw new BadRequestException('The username is already in use');
+    }
+
+    const user = this.usersRepository.create({
+      ...createUserDto,
+      role: 'board',
+    });
+
+    return await this.usersRepository.save(user);
   }
 }
